830a94b6f4706f9ba7711593c00d928cd2b28f62
mike <mike@gmail.com>
2025-05-20 17:01:57 +0200

    feat(backend): added bus related classes

--- backend/src/meetup/domain/events/MeetupImageUrlUpdatedDomainEvent.ts
+++ backend/src/meetup/domain/events/MeetupImageUrlUpdatedDomainEvent.ts
@@ -1,4 +1,4 @@
-import { DomainEvent } from "@/shared/domain/events/DomainEvent"
+import { DomainEvent } from "@/shared/domain/DomainEvent"
 
 type MeetupImageUrlUpdatedDomainEventAttributes = {
   readonly imageUrl: string
