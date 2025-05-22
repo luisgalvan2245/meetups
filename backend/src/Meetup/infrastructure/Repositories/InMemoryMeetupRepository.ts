8544b56951ca26d7cca7a6a0fb61a19d5d08fa90
mike <mike@gmail.com>
2025-05-22 07:56:39 +0200

    refactor(backend): encapsulate fake db in function

--- backend/src/Meetup/infrastructure/Repositories/InMemoryMeetupRepository.ts
+++ backend/src/Meetup/infrastructure/Repositories/InMemoryMeetupRepository.ts
@@ -3,38 +3,42 @@ import { Meetup } from "../../domain/Meetup"
 import { MeetupRepository } from "../../domain/Repositories/MeetupRepository"
 import { v4 as uuidV4 } from "uuid"
 
-const futureDate = new Date()
-futureDate.setDate(futureDate.getDate() + 7)
+function createMeetups() {
+  const futureDate = new Date()
+  futureDate.setDate(futureDate.getDate() + 7)
 
-const meetup1 = Meetup.fromPrimitives({
-  id: uuidV4(),
-  title: "Meetup 1",
-  description: "Description 1",
-  date: futureDate.toISOString(),
-  location: "Location 1",
-  imageUrl: "https://example.com/1.jpg"
-})
+  const meetup1 = Meetup.fromPrimitives({
+    id: uuidV4(),
+    title: "Meetup 1",
+    description: "Description 1",
+    date: futureDate.toISOString(),
+    location: "Location 1",
+    imageUrl: "https://example.com/1.jpg"
+  })
 
-const meetup2 = Meetup.fromPrimitives({
-  id: uuidV4(),
-  title: "Meetup 2",
-  description: "Description 2",
-  date: futureDate.toISOString(),
-  location: "Location 2",
-  imageUrl: "https://example.com/2.jpg"
-})
+  const meetup2 = Meetup.fromPrimitives({
+    id: uuidV4(),
+    title: "Meetup 2",
+    description: "Description 2",
+    date: futureDate.toISOString(),
+    location: "Location 2",
+    imageUrl: "https://example.com/2.jpg"
+  })
 
-const meetup3 = Meetup.fromPrimitives({
-  id: uuidV4(),
-  title: "Meetup 3",
-  description: "Description 3",
-  date: futureDate.toISOString(),
-  location: "Location 3",
-  imageUrl: "https://example.com/3.jpg"
-})
+  const meetup3 = Meetup.fromPrimitives({
+    id: uuidV4(),
+    title: "Meetup 3",
+    description: "Description 3",
+    date: futureDate.toISOString(),
+    location: "Location 3",
+    imageUrl: "https://example.com/3.jpg"
+  })
+
+  return [meetup1, meetup2, meetup3]
+}
 
 export class InMemoryMeetupRepository implements MeetupRepository {
-  private static meetups: Meetup[] = [meetup1, meetup2, meetup3]
+  private static meetups: Meetup[] = createMeetups()
 
   async findAll(): Promise<Meetup[]> {
     return InMemoryMeetupRepository.meetups
