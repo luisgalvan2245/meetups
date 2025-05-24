a01eae561d892e0444c8b0423b3163ac7de006ab
mike <mike@gmail.com>
2025-05-24 10:33:13 +0200

    chore(backend): exclude .config.ts in eslint

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -2,31 +2,19 @@ import tsPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import { FlatESLintConfig } from 'eslint-define-config'
 
+console.log('✅ ESLint config loaded')
+
 const config: FlatESLintConfig[] = [
   {
+    ignores: ['**/routes.ts', '*.config.ts']
+  },
+  {
     files: ['**/*.ts', '**/*.tsx'],
-    ignores: ['**/*.config.ts', '**/routes.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         sourceType: 'module',
         project: ['./tsconfig.json', './tsconfig.*.json']
-      }
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin as any
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      'no-unused-vars': 'off'
-    }
-  },
-  {
-    files: ['**/*.config.ts'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        sourceType: 'module'
       }
     },
     plugins: {
