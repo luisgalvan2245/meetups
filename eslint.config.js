dca7212bc1881be270aaa9f5be8200c799e9c4eb
mike <mike@gmail.com>
2025-05-24 09:30:16 +0200

    build(backend): install eslint and dependencies

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// eslint.config.js
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+      import: require("eslint-plugin-import")
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["parent", "sibling", "index"]],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true }
+        }
+      ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"]
+    }
+  }
+];
