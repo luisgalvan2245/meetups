77be764163d2fb4f3a7a53867d00223db697af9e
mike <mike@gmail.com>
2025-05-25 09:04:59 +0200

    fix(backend): fix UserId imports

--- backend/src/Shared/domain/Exceptions/InvalidPasswordFormatError.ts
+++ backend/src/Shared/domain/Exceptions/InvalidPasswordFormatError.ts
@@ -3,6 +3,5 @@ import { FormatError } from './FormatError'
 export class InvalidPasswordFormatError extends FormatError {
   constructor(message: string) {
     super(message)
-    this.name = 'InvalidPasswordFormatError'
   }
 }
--- backend/src/User/domain/ValueObjects/UserId.ts
+++ backend/src/User/domain/ValueObjects/UserId.ts
@@ -1,7 +1,21 @@
+import { v4 as uuidV4 } from 'uuid'
+
+import { NotNullError } from '../../../Shared/domain/Exceptions/NotNullError'
 import { UUIDValueObject } from '../../../Shared/domain/ValueObjects/UUIDValueObject'
 
 export class UserId extends UUIDValueObject {
   constructor(value: string) {
     super(value)
+  }
+
+  static generate(): UserId {
+    return new UserId(uuidV4())
+  }
+
+  static create(value: string): UserId {
+    if (!value) {
+      throw new NotNullError('User ID cannot be empty')
+    }
+    return new UserId(value)
   }
 }
