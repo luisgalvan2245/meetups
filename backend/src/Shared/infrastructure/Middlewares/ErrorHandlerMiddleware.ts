1b7e880639960c7e116bec252f17ff33fad8f749
mike <mike@gmail.com>
2025-05-26 10:13:29 +0200

    refactor(backend): remove logs in middleware

--- backend/src/Shared/infrastructure/Middlewares/ErrorHandlerMiddleware.ts
+++ backend/src/Shared/infrastructure/Middlewares/ErrorHandlerMiddleware.ts
@@ -14,26 +14,6 @@ type ErrorResponse = {
 
 export class ErrorHandlerMiddleware {
   static handle(err: Error, _req: Request, res: Response, _next: NextFunction) {
-    console.error('[ErrorHandlerMiddleware] Received error:', err)
-    console.error('[ErrorHandlerMiddleware] Error name:', err?.name)
-    console.error('[ErrorHandlerMiddleware] Error message:', err?.message)
-    console.error(
-      '[ErrorHandlerMiddleware] err instanceof FormatError:',
-      err instanceof FormatError
-    )
-    console.error(
-      '[ErrorHandlerMiddleware] err instanceof NotFoundError:',
-      err instanceof NotFoundError
-    )
-    console.error(
-      '[ErrorHandlerMiddleware] err instanceof BusinessRuleError:',
-      err instanceof BusinessRuleError
-    )
-    console.error(
-      '[ErrorHandlerMiddleware] err instanceof DomainError:',
-      err instanceof DomainError
-    )
-
     let statusCode: number = status.INTERNAL_SERVER_ERROR
     const response: ErrorResponse = {
       message: 'Internal Server Error'
@@ -57,6 +37,6 @@ export class ErrorHandlerMiddleware {
       response.message = err.message
     }
 
-    return res.status(statusCode).json(response)
+    res.status(statusCode).json(response)
   }
 }
